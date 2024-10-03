de1d660693c62a83be0e49fa9458df65c3c07516
pwwww-n <150579399+pwwww-n@users.noreply.github.com>
2024-10-03 12:44:30 +0530

    Create symlink.sh

---
+++ symlink.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# symlink.sh
+
+ln -sf ~/MyDotfiles/.bashrc ~/.bashrc
+ln -sf ~/MyDotfiles/.vimrc ~/.vimrc
+ln -sf ~/MyDotfiles/.gitconfig ~/.gitconfig
+
+echo "Symlinks created successfully!"
